8d6668f694f29434a0ab93de4a9be47bb2ff54bd
Hadeel Obaid <hadeelobaid71@gmail.com>
2024-03-02 03:19:57 +0300

    Practice

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,16 +1,93 @@
-import React, { useState } from "react";
+// src/components/Form
+import React from "react";
 
-function Form() {
-  const [firstName, setFirstName] = useState("John");
-  const [lastName, setLastName] = useState("Henry");
-
+function Form({
+  firstName,
+  lastName,
+  handleFirstNameChange,
+  handleLastNameChange,
+}) {
   return (
     <form>
-      <input type="text" value={firstName} />
-      <input type="text" value={lastName} />
+      <input type="text" onChange={handleFirstNameChange} value={firstName} />
+      <input type="text" onChange={handleLastNameChange} value={lastName} />
       <button type="submit">Submit</button>
     </form>
   );
 }
 
 export default Form;
+
+// function Form() {
+//   const [number, setNumber] = useState(0);
+//   const [isInvalidNumber, setIsInvalidNumber] = useState(null);
+
+//   function handleNumberChange(event) {
+//     const newNumber = parseInt(event.target.value);
+//     if (newNumber >= 0 && newNumber <= 5) {
+//       setNumber(newNumber);
+//       setIsInvalidNumber(null);
+//     } else {
+//       setIsInvalidNumber(`${newNumber} is not a valid number!`);
+//     }
+//   }
+
+//   return (
+//     <form>
+//       <input type="number" value={number} onChange={handleNumberChange} />
+//       {isInvalidNumber ? <span style={{ color: "red" }}>{isInvalidNumber}</span> : null}
+//     </form>
+//   );
+// }
+
+// import React, { useState } from "react";
+
+// function Form() {
+//   const [newsletter, setNewsletter] = useState(false);
+
+//   function handleNewsletterChange(event) {
+//     // .checked, not .value!
+//     setNewsletter(event.target.checked);
+//   }
+
+//   return (
+//     <form>
+//       <label htmlFor="newsletter">Subscribe to our Newsletter?</label>
+//       <input
+//         type="checkbox"
+//         id="newsletter"
+//         onChange={handleNewsletterChange}
+//         {/* checked instead of value */}
+//         checked={newsletter}
+//       />
+//       <button type="submit">Submit</button>
+//     </form>
+//   );
+// }
+
+// export default Form;
+
+// import React, { useState } from "react";
+
+// function Form() {
+//   const [firstName, setFirstName] = useState("John");
+//   const [lastName, setLastName] = useState("Henry");
+
+//   function handleFirstNameChange(event) {
+//     setFirstName(event.target.value);
+//   }
+
+//   function handleLastNameChange(event) {
+//     setLastName(event.target.value);
+//   }
+
+//   return (
+//     <form>
+//       <input type="text" onChange={handleFirstNameChange} value={firstName} />
+//       <input type="text" onChange={handleLastNameChange} value={lastName} />
+//       <button type="submit">Submit</button>
+//     </form>
+//   );
+// }
+
+// export default Form;
